68d54a0bd59b455c170221363feaee316db5c571
Gopal Krishan <50587512+gopalkbks@users.noreply.github.com>
2019-06-05 20:26:58 -0400

    Update variables.php

--- variables.php
+++ variables.php
@@ -1,3 +1,5 @@
+<?php
+
 /*
 +---+
 | 1 |
@@ -5,6 +7,8 @@
 Declare the variable named language and 
 assign it with value PHP.
 */
+
+$language = "PHP";
 
 /*
 Combine (chain or concatenate) language 
@@ -16,11 +20,13 @@ Declare the variable greeting,
 assign it with the created expression.
 */
 
+$greeting = 'Welcome to'.$language.'!.';
+
 /*
 Print greeting.
 */
 
-
+echo $greeting;
 
 
 // task separator
@@ -34,25 +40,35 @@ Declare variable named firstName and
 assign it with the value of your first name.
 */
 
+$firstName ="Gopal";
+
 /*  
 Declare variable named lastName and 
 assign it with the value of your last name.
 */
 
+$lastName = "Krishan";
+
 /*
 Declare variable named day and 
 assign it with the day of your birthday (number).
 */
+    
+$day = 1;
 
 /*
 Declare variable named month and 
 assign it with the month of your birthday (string).
 */
 
+$month = "June";
+
 /*
 Declare variable named year and 
 assign it with the year of your birthday (number).
 */
+
+$year = 1996;
 
 /*
 Use all of the variables above and create expression that will
@@ -63,11 +79,13 @@ Declare variable named assembled and
 assign it with the chained (concatenated) expression above.
 */
 
+$assembled = "My name is"." ".$firstName." ".$lastName."."."I was born on"." ".$day."of"." ".$month." "."in"." " .$year.".";
+
 /*
 Print assembled.
 */    
 
-
+echo $assembled;
 
 
 // task separator
@@ -87,17 +105,25 @@ Calculate and print the area of the rectangle below:
 Declare the variable length and assign it with the specified value.
 */
 
+$length = "3.5";
+
 /*
 Declare the variable width and assign it with the specified value.
 */
+
+$width = "6.5";
 
 /*
 Declare the variable area and assign it with the product of length and width.
 */
 
+$area = $length * $width;
+
 /*
 Print area.
 */
+
+echo $area;
 
 
 
@@ -116,17 +142,25 @@ Description: Printing the price of product including the tax and delivery.
 Declare the variable product and assign it with armchair.
 */
 
+$product = "armchair";
+
 /*
 Declare the variable price and assign it with 249.00.
 */
+
+$price = 249.00;
 
 /*
 Declare the variable tax and assign it with 13% (0.13).
 */
 
+$tax = 0.13;
+
 /*
 Declare the variable delivery and assign it with 5% (0.05).
 */
+
+$delivery = 0.05;
 
 /*
 Use price, tax and delivery 
@@ -135,12 +169,15 @@ to create the expression that will output the price after the tax and delivery.
 Declare the variable total and assign it with the expression gotten from the previous step.
 */
 
+$total = $price + $price * $tax;
+$total = $total + $total * $delivery;
+
 /*
 Concatenate the variables and string literals to print the statement:
 armchair: $295.44
 */
 
-
+echo $product .":"." "."$".round($total,2);
 
 
 // task separator
@@ -158,3 +195,16 @@ should be contained by figcaption element):
   <figcaption> ... </figcaption>
 </figure>
 */
+
+?>
+
+<html>
+    <figure>
+  <img src="https://placehold.jp/24/e8d2ae/fff/300x300.png" alt="placeholder-image">
+  <figcaption> 
+  <?php
+    echo $product .":"." "."$".round($total,2);  
+  ?>
+  </figcaption>
+</figure>
+</html>
